5044a3aae83dd25bcea1fe9d1aec6c2feb127d4a
Leonardo Fonseca <leofonseca.magalhaes@gmail.com>
2022-08-05 16:08:10 -0300

    projetos Utilitarios

---
+++ contadorDeLinhas/src/contadorDeLinhas/Contador.java
@@ -0,0 +1,30 @@
+package contadorDeLinhas;
+
+import java.io.FileReader;
+import java.io.LineNumberReader;
+
+public class Contador {
+		
+	public static void main(String[] args) {
+		LineNumberReader lineNumberReader = null;
+		FileReader fileReader = null;
+		
+		try {
+			fileReader = new FileReader("D:\\arq\\fiel.txt");
+			lineNumberReader = new LineNumberReader(fileReader);
+			String line;
+			
+			while((line =lineNumberReader.readLine()) != null) {
+				lineNumberReader.skip(Long.MAX_VALUE);
+				int lineNumber =lineNumberReader.getLineNumber();
+			
+				System.out.println(lineNumber);
+			}
+			fileReader.close();
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
